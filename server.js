1aa4102cec01d3a27e6c3dbf6caed776c27764e0
hiramreyes06 <luishiramreyes@hotmail.com>
2021-09-27 23:28:55 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => res.send('Hola mundo desde la app de node'));
+
+app.listen(3000);
+console.log('Servidor corriendo en el puerto 3000');
